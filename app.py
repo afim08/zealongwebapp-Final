60c4e8b4fa8aea5a0d9e2e2072d899500221865b
Afi Cherian <afim08@gmail.com>
2022-11-23 08:07:30 +1300

    test2

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flaskblog import create_app
+from flask import Flask, request, app, jsonify, url_for, render_template 
 
-app= create_app()
+app=Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route("/about")  
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
