f4c4ab9f556cfcc287d74268b24b10b22db74ec2
Afi Cherian <afim08@gmail.com>
2022-11-25 08:42:55 +1300

    tea names fetched

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, app, jsonify, url_for, render_template 
+import pandas as pd 
 
 app=Flask(__name__)
 
@@ -9,6 +10,26 @@ def home():
 @app.route("/about")  
 def about():
     return render_template('about.html')
+
+
+@app.route('/schedule',methods=['GET', 'POST'])
+def schedule():
+    
+    df=[]
+    tea1 = request.form.get("Tea1")
+    df.append(tea1)
+    tea2 = request.form.get("Tea2")
+    df.append(tea2)
+    tea3 = request.form.get("Tea3")
+    df.append(tea3)
+    df = pd.DataFrame()
+
+    
+   
+   
+    
+    
+    return render_template('home.html', schedule_text = "The schedule may start from  {}" .format(df))
 
 if __name__ == '__main__':
     app.run(debug=True)
