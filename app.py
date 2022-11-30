837df9e93dea764c3ac043fb7b3fb5532fd287ef
Afi Cherian <afim08@gmail.com>
2022-11-30 17:30:36 +1300

    Upload and Schedule Done

--- app.py
+++ app.py
@@ -99,26 +99,189 @@ def Data():
 
 @app.route('/schedule',methods=['GET', 'POST'])
 def schedule():
-    
-    df_tea=[]
-    df_qty=[]
-    tea1 = request.form.get("Tea1")
-    df_tea.append(tea1)
-    tea2 = request.form.get("Tea2")
-    df_tea.append(tea2)
-    tea3 = request.form.get("Tea3")
-    df_tea.append(tea3)
+        import pandas as pd
+        import math
 
-    teaqty1 = request.form.get("Teacount1")
-    df_qty.append(teaqty1)
-    teaqty2 = request.form.get("Teacount2")
-    df_qty.append(teaqty2)
-    teaqty3 = request.form.get("Teacount3")
-    df_qty.append(teaqty3)
-    
-    #schedulefunction(df_tea,df_qty)
-    
-    return render_template('home.html', schedule_text = "The best production schedule order is   {} and {}" .format(df1_1,df2))
+        #defining all different teas and their classifications 
+        origins = ['Oolong','Green','Black'] 
+        black_blend = ['Black','ZBT','FI','Chai','SA','Grey','CHM','ChinaGrey']
+        green_blend = ['Green','LG','IB']
+        oolong_blend= ['Oolong','GH']
+        no_tea =['RG','CHCF']
+        huami =['Jasmine','HuamiOolong','HuamiBlack','HuamiGreen']
+        tea_2g = ['sCHCF','sOolong','sGreen','sBlack','sZBT','sChai','sSA','sGrey','sLG','sIB','sGH']
+        sachets= ['CHCF','Oolong','Green','Black','ZBT','Chai','SA','Grey','LG','IB','GH']
+        non_woven=['SA','Chai','ZBT','RG','sZBT','sChai','sSA']
+        on_csk = ['Oolong','Green','Black','FI','Grey','CHM','ChinaGrey','LG'
+                ,'IB','GH','RG','CHCF','sCHCF','sOolong','sGreen','sBlack','sGrey','sLG','sIB','sGH']
+        on_csz = ['Jasmine','HuamiOolong','HuamiBlack','HuamiGreen','SA','Chai','ZBT','RG']
 
+        all_tea_dict = {"black_blend": ["Black",'ZBT','FI','Chai','SA','Grey','CHM','ChinaGrey','sBlack','sGrey'],
+                        "green_blend" : ["Green",'LG','IB','sGreen','sLG','sIB'],"oolong_blend": ["Oolong",'GH','sOolong','sGH'],"no_tea" :['RG','CHCF','sCHCF'],
+                        "huami" :['Jasmine','HuamiOolong','HuamiBlack','HuamiGreen']}
+
+
+        #defining class for classification of tea based on machine and filter
+        class TeaType:
+            def __init__(self,name):
+                self.name=name
+                TeaType.tea_origin(self.name)
+                TeaType.tea_filter(self.name)
+
+            @classmethod
+            def tea_origin(self,inp):
+
+                for x,y in all_tea_dict.items():
+                    for t in y:
+                        if inp==t:
+                            return x
+
+            @classmethod
+            def tea_filter(self,inp):
+
+                if inp in non_woven:
+                    return False
+                else:
+                    return True
+
+            @classmethod
+            def tea_machine(self,inp):
+                if inp in on_csk:
+                    return 'csk'
+                else:
+                    return "csz"
+
+
+        # csv file taken as dataframe and 
+        # sorted on the basis of machine 
+        df = pd.DataFrame(columns=["TEA","COUNT"])
+        df = pd.read_csv('./static/uploads/abc.csv')
+        machine_csz=[]
+        machine_csz_count=[]
+        machine_csk=[]
+        machine_csk_count=[]
+        for i in range(len(df.TEA)):
+            c=TeaType.tea_machine(df.TEA[i])
+            if c=='csz':
+                machine_csz.append(df.TEA[i])
+                machine_csz_count.append(df.COUNT[i])
+            else:
+                machine_csk.append(df.TEA[i])
+                machine_csk_count.append(df.COUNT[i])
+
+        # Teas from the sheet is fetched and sorted on the basis of CS-Z and CS-K 
+
+        # we will try to sort the Tea order now 
+        #first consider the csk teas
+        df1 =pd.DataFrame()
+        df1["CSK"] =pd.Series(machine_csk)
+        df1["COUNT"]=pd.Series(machine_csk_count)
+
+        #sorting cs-z
+        df2 =pd.DataFrame()
+        df2["CSZ"] =pd.Series(machine_csz)
+        df2["COUNT"]=pd.Series(machine_csz_count)
+        new2=[]
+        for i in range(len(df2.CSZ)):
+            b=TeaType.tea_origin(df2.CSZ[i])
+            new2.append(b)
+        df2["Origin"] =new2
+
+        #adding the origin of each tea as a column 
+
+        new=[]
+        for i in range(len(df1.CSK)):
+            a=TeaType.tea_origin(df1.CSK[i])
+            new.append(a)
+        df1["Origin"] =new
+
+
+        df1_1 =pd.DataFrame(columns=df1.columns)# make a new df with same headings
+
+        m=len(df1.index)
+        index_lst=[]
+        for i in range(m):
+            index_lst.append(df1.index[i])
+
+        l = 5 if len(df1.index) > 5 else len(df1.index)
+
+        comp_list=[]
+        for i in index_lst:
+            comp_list.append(df1.loc[i][2])
+
+        total_num= len(df1.index)
+
+
+        a=df1.loc[index_lst[0]]
+        df1_1= df1_1.append(a)
+        df1.drop(index_lst[0],axis="index",inplace=True)
+
+
+        while len(df1_1.index)<total_num:
+
+
+            m=len(df1.index)
+            index_lst=[]
+            for i in range(m):
+                index_lst.append(df1.index[i])
+
+
+
+            l = 5 if len(df1.index) > 5 else len(df1.index)
+
+
+
+
+            comp_list=[]
+            for i in index_lst:
+                comp_list.append(df1.loc[i][2])
+
+
+
+
+            n=len(df1_1.index)
+            index_lst_temp=[]
+            for i in range(n):
+                index_lst_temp.append(df1_1.index[i])
+
+            b=index_lst_temp[-1]
+
+            for i in index_lst[0:l]:
+                if df1_1.loc[b][2]== df1.loc[i][2]:
+                    t= df1.loc[i]
+                    df1_1= df1_1.append(t)
+                    df1.drop(i,axis="index",inplace=True)
+                else:
+                    pass 
+
+
+            if df1_1.index[-1]==b:
+
+                df1_1=df1_1.append(df1.loc[index_lst[0]])
+                try:
+                    df1_1=df1_1.append(df1.loc[index_lst[1]])
+                except:
+                    pass
+                try:
+                    df1.drop(index_lst[0],axis="index",inplace=True)
+                except:
+                    pass
+                try:
+                    df1.drop(index_lst[1],axis="index",inplace=True)
+                except:
+                    pass
+
+
+
+
+    
+        
+
+        df1_display = df1_1.to_html(classes='table table-stripped')
+
+    
+        return render_template('schedule.html', data_var5=df1_display)
+ 
+ 
 if __name__ == '__main__':
     app.run(debug=True)
